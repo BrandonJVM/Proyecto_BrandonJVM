d8a8d7419d744682001b1c353167cdd30949f5f5
Brandon <bvargas40854@ufide.ac.cr>
2024-10-08 18:57:24 -0600

    update del codigo
    Update del codigo ya funcinoanande en web

---
+++ Proyecto_BrandonJVM/src/main/java/com/example/Proyecto_BrandonJVM/ProyectoBrandonJvmApplication.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_BrandonJVM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBrandonJvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBrandonJvmApplication.class, args);
+	}
+
+}
---
+++ Proyecto_BrandonJVM/src/test/java/com/example/Proyecto_BrandonJVM/ProyectoBrandonJvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto_BrandonJVM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBrandonJvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
