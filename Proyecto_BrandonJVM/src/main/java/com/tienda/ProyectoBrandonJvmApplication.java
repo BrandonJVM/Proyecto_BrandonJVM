b447d76d3ce2e27d036e8f01b67e1f98ff32c33e
Brandon <bvargas40854@ufide.ac.cr>
2024-11-05 21:04:13 -0600

    update

---
+++ Proyecto_BrandonJVM/src/main/java/com/tienda/ProyectoBrandonJvmApplication.java
@@ -0,0 +1,13 @@
+package com.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBrandonJvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBrandonJvmApplication.class, args);
+	}
+
+}
